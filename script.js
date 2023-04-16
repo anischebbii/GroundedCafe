4168bc4e29dbf86e36c834354c9b132eaf85d9a1
anischebbii <anis.chebbi@outlook.com>
2023-04-17 00:55:49 +0100

    VS code auto-formatting

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
     setActiveMenuItem();
 
     function setActiveMenuItem() {
-    const currentUrl = window.location.href;
-    const navLinks = document.querySelectorAll(".nav-link");
+        const currentUrl = window.location.href;
+        const navLinks = document.querySelectorAll(".nav-link");
 
-    navLinks.forEach((navLink) => {
-        if (navLink.href === currentUrl) {
-        navLink.classList.add("active");
-        } else {
-        navLink.classList.remove("active");
-        }
-    });
+        navLinks.forEach((navLink) => {
+            if (navLink.href === currentUrl) {
+                navLink.classList.add("active");
+            } else {
+                navLink.classList.remove("active");
+            }
+        });
     }
 });
 
 var nav = document.querySelector('nav');
-        
-        window.addEventListener('scroll', function () {
-            if (window.pageYOffset > 170) {
-                nav.classList.add('bg-dark', 'shadow');
-            } else {
-                nav.classList.remove('bg-dark', 'shadow');
-            }
-        });
-        const navbarToggler = document.querySelector(".navbar-toggler");
-        navbarToggler.addEventListener("click", function () {
-          nav.classList.toggle("bg-dark");
-          nav.classList.toggle("shadow");
-        });
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 170) {
+        nav.classList.add('bg-dark', 'shadow');
+    } else {
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+});
+const navbarToggler = document.querySelector(".navbar-toggler");
+navbarToggler.addEventListener("click", function () {
+    nav.classList.toggle("bg-dark");
+    nav.classList.toggle("shadow");
+});
 
 
 function updateFormFields() {
@@ -46,7 +46,7 @@ function validateEmail() {
     const emailInput = document.getElementById("email");
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const isValidEmail = emailPattern.test(emailInput.value);
-    
+
     if (isValidEmail) {
         emailInput.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
     } else {
